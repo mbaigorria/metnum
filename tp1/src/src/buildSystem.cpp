c629224804a12ed9913bb42a8eef18e6bfe64647
mbaigorria <martinbaigorria@gmail.com>
2015-08-18 13:09:31 -0300

    System.

--- tp1/src/src/buildSystem.cpp
+++ tp1/src/src/buildSystem.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <math.h>
 
+#include "eqsys.h"
+
 using namespace std;
 
-void insertValue(double* A, double* b, int j, int k, double r_i, double r_e, int m, int n, double t_e);
+void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i, double r_e, int m, int n, double t_e);
 
 int main() {
 
@@ -18,10 +20,10 @@ int main() {
 	double t_e = 1000;
 
 	// build system: Ax = b
-	double A[(m-1)*(n+1)][(m-1)*(n+1)] = {0};
-	double b[(m-1)*(n+1)] = {0};
+	Matrix<double> A((m-1)*(n+1),(m-1)*(n+1));
+	Matrix<double> b((m-1)*(n+1));
 
-	/* each temperature has 1 laplacian, and depends on at least 3 temperatures.
+	/* each temperature has 1 laplacian, and depends on 4 temperatures.
 	 * i'm looking for t_j,k in the valid range.
 	 */ 
 	for (int k = 0; k <= n; k++) {
@@ -44,29 +46,29 @@ int main() {
  *     | ...... |
  *     | tm-1,n |
  */
-void insertValue(double* A, double* b, int j, int k, double r_i, double r_e, int m, int n, double t_e) {
+void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i, double r_e, int m, int n, double t_e) {
 
-	double dO = M_PI / n;
+	double dO = 2*M_PI / n;
 	double dR = (r_e - r_i) / k;
 	int r = (j-1) * (n+1);
 	double r_j = r_i + j*dR;
 
-	A[r][k*m + (j-1)]     = - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2)); // t_j,k
+	A(r,k*m + (j-1))     = - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2)); // t_j,k
 
-	A[r][k*m + (j-1) + 1] = (1/pow(dR, 2)); // t_j+1,k
+	A(r,k*m + (j-1) + 1) = (1/pow(dR, 2)); // t_j+1,k
 
-	A[r][((k-1) % (n+1)) * m + (j-1)] = (1/(pow(r_j, 2)*pow(dO, 2))); // t_j,k-1, border case! k < 0
+	A(r,((k-1) % (n+1)) * m + (j-1)) = (1/(pow(r_j, 2)*pow(dO, 2))); // t_j,k-1, border case! k < 0
 
 	if (k == 1) { // inner circle
-		A[r][(j-1)*n + j - 1] = (1/pow(dR, 2)) + 1 / (r_j * dR); // t_j-1,k
-		b[r] -= 1/pow(dR,2) * 1500;
+		A(r,(j-1)*n + j - 1) = (1/pow(dR, 2)) + 1 / (r_j * dR); // t_j-1,k
+		b(r) -= 1/pow(dR,2) * 1500;
 
 	} else if (k == m) { // outer circle
-		A[r][(j-1)*n + j - 1] = (1/pow(dR, 2)) + 1 / (r_j * dR); // t_j-1,k
-		b[r] -= 1/pow(dR,2) * t_e;
+		A(r,(j-1)*n + j - 1) = (1/pow(dR, 2)) + 1 / (r_j * dR); // t_j-1,k
+		b(r) -= 1/pow(dR,2) * t_e;
 
 	} else {
-		A[r][(j-1)*n + j - 1] = 1 / (r_j * dR); // t_j-1,k
+		A(r,(j-1)*n + j - 1) = 1 / (r_j * dR); // t_j-1,k
 	}
 
 }
