df5bbd3a31a3aa80f955dea3b7a1b7617dbf05f2
mbaigorria <martinbaigorria@gmail.com>
2015-08-19 13:20:48 -0300

    Changes.

--- tp1/src/src/buildSystem.cpp
+++ tp1/src/src/buildSystem.cpp
@@ -11,8 +11,8 @@ void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i,
 int main() {
 
 	// granularity
-	int n = 1; // O0 < 0_k < ... < 0_n
-	int m = 2; // r0 < r_j < ... < r_m 
+	int n = 2; // O0 < 0_k < ... < 0_n
+	int m = 3; // r0 < r_j < ... < r_m 
 
 	// system parameters
 	double r_i = 1;
@@ -33,6 +33,7 @@ int main() {
 		}
 	}
 
+	cout << endl;
 	cout << "Matrix A" << endl;
 	A.printMatrix();
 
@@ -55,6 +56,8 @@ int main() {
  */
 void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i, double r_e, int n, int m, double t_e) {
 
+	cout << "j: " << j << " k: " << k << " m: " << m << " n: " << n << endl;
+
 	double dO = 2*M_PI / (n+1);
 	double dR = (r_e - r_i) / m;
 
@@ -62,26 +65,26 @@ void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i,
 	double r_j = r_i + j*dR;
 
 	// t_j,k
-	A(r,r)     = - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2));
+	A(r,r) += - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2));
 
 	// t_j,k+1, border case! k > n, angle = 0
-	A(r, r + (j-1) ) = - (2/pow(r_j, 2)*pow(dO, 2));
+	A(r, (r + (m-1)) % (m-1)*(n+1)) += - (2/pow(r_j, 2)*pow(dO, 2));
 
 	// t_j,k-1, border case! k < 0 
-	A(r,(r - (m-1)) % (m-1)*(n+1)) = (1/(pow(r_j, 2)*pow(dO, 2)));
+	A(r,(r - (m-1)) % (m-1)*(n+1))  += (1/(pow(r_j, 2)*pow(dO, 2)));
 
 	// t_j-1,k
 	if (j == 1) { // inner circle
-		b(r) = INNER_TEMP * ((2/pow(dR, 2)) + (1 / (r_e * dR)));
+		b(r) += INNER_TEMP * ((2/pow(dR, 2)) + (1 / (r_e * dR)));
 	} else {
-		A(r,r - 1) = - (2/pow(dR, 2)) - (1 / (r_j * dR));
+		A(r,r - 1) += - (2/pow(dR, 2)) - (1 / (r_j * dR));
 	}
 	
 	// t_j+1,k
 	if (j+1 == m) { // outer circle
-		b(r) = t_e * (2/pow(dR, 2));
+		b(r) += t_e * (2/pow(dR, 2));
 	} else {
-		A(r,r + 1) = (-2/pow(dR, 2));
+		A(r,r + 1) += (-2/pow(dR, 2));
 	}
 
 }
