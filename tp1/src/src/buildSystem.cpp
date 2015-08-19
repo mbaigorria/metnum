fe61163f74e1849445db7eb42a4d8ff8185d5a2a
mbaigorria <martinbaigorria@gmail.com>
2015-08-19 12:35:22 -0300

    Changes.

--- tp1/src/src/buildSystem.cpp
+++ tp1/src/src/buildSystem.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "eqsys.h"
 
-#define INNER_TEMP 1500
+#define INNER_TEMP 20
 
 using namespace std;
 
@@ -11,14 +11,14 @@ void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i,
 int main() {
 
 	// granularity
-	int n = 3; // O0 < 0_k < ... < 0_n
-	int m = 3; // r0 < r_j < ... < r_m 
+	int n = 1; // O0 < 0_k < ... < 0_n
+	int m = 2; // r0 < r_j < ... < r_m 
 
 	// system parameters
-	double r_i = 10;
+	double r_i = 1;
 
-	double r_e = 20;
-	double t_e = 1000;
+	double r_e = 3;
+	double t_e = 10;
 
 	// build system: Ax = b
 	Matrix<double> A((m-1)*(n+1),(m-1)*(n+1));
@@ -55,25 +55,31 @@ int main() {
  */
 void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i, double r_e, int n, int m, double t_e) {
 
-	double dO = 2*M_PI / n;
+	double dO = 2*M_PI / (n+1);
 	double dR = (r_e - r_i) / m;
 
 	int r = k * (m - 1) + (j - 1);
 	double r_j = r_i + j*dR;
 
-	A(r,r)     = - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2)); // t_j,k
+	// t_j,k
+	A(r,r)     = - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2));
 
-	A(r,r + 1) = (1/pow(dR, 2)); // t_j+1,k
+	// t_j,k+1, border case! k > n, angle = 0
+	A(r, (j-1)) = - (2/pow(r_j, 2)*pow(dO, 2));
 
-	A(r,(r - (m-1)) % (m-1)*(n+1)) = (1/(pow(r_j, 2)*pow(dO, 2))); // t_j,k-1, border case! k < 0
+	// t_j,k-1, border case! k < 0 
+	A(r,(r - (m-1)) % (m-1)*(n+1)) = (1/(pow(r_j, 2)*pow(dO, 2)));
+
+	// t_j+1,k
+	A(r,r + 1) = (-2/pow(dR, 2));
 
 	// t_j-1,k
 	if (j == 1) { // inner circle
-		b(r) = INNER_TEMP * (1/pow(dR,2) + 1/(r_e*dR));
+		b(r) = INNER_TEMP * ((2/pow(dR, 2)) + (1 / (r_e * dR)));
 	} else if (j == m) { // outer circle
-		b(r) = t_e * (1/pow(dR,2) + 1/(r_e*dR));
+		b(r) = t_e * ((2/pow(dR, 2)) + (1 / (r_e * dR)));
 	} else {
-		A(r,r - 1) = (1 / pow(r_j, 2)) + (1 / (r_j * dR));
+		A(r,r - 1) = - (2/pow(dR, 2)) - (1 / (r_j * dR));
 	}
 
 }
