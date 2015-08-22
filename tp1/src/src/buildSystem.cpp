400bd8cd17dee42188a457cb22b8219797609886
McCoy2X <federicobeuter@gmail.com>
2015-08-21 21:02:36 -0300

    Added fixes to buildSystem.cpp

--- tp1/src/src/buildSystem.cpp
+++ tp1/src/src/buildSystem.cpp
@@ -26,11 +26,11 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-	printf("Let's get started!\n");
+	// printf("Let's get started!\n");
 
 	// granularity
-	int n; // O0 < 0_k < ... < 0_n
-	int m; // r0 < r_j < ... < r_m 
+	int n; // 0 = O0 < 0_k < ... < 0_n = 2PI
+	int m; // ri = r0 < r_j < ... < r_m = re 
 
 	// system parameters
 	double r_i, r_e;
@@ -46,7 +46,7 @@ int main(int argc, char** argv) {
 	getline(inputFile, line);
 	sscanf(line.c_str(),"%lf %lf %d %d %lf %d",&r_i,&r_e,&m,&n,&iso,&ninst);
 
-	cout << "r_i: " << r_i << " r_e: " << r_e << " m: " << m << " n: " << n << " iso: " << iso << " ninst: " << ninst << endl;
+	cout << "r_i: " << r_i << " r_e: " << r_e << " m+1: " << m << " n: " << n << " iso: " << iso << " ninst: " << ninst << endl;
 
 	// load temperatures (one instance for now)
 	getline(inputFile, line);
@@ -66,27 +66,27 @@ int main(int argc, char** argv) {
 	inputFile.close();
 
 	// build system: Ax = b
-	Matrix<double> A((m-2)*(n+1),(m-2)*(n+1),0);
-	Matrix<double> b((m-2)*(n+1),1,0);
+	Matrix<double> A(n*m,n*m,0);
+	Matrix<double> b(n*m,1,0);
 
 	// each temperature has 1 laplacian, and depends on 4 temperatures.
 	// i'm looking for t_j,k in the valid range.
 	
-	printf("Loading system!\n");
-	for (int k = 0; k <= n; k++) {
-		for (int j = 1; j < m-1; j++) { // avoid borders
-			insertValue(A,b,j,k,r_i,r_e,n,m-1,t_i,t_e);
+	// printf("Loading system!\n");
+	for (int j = 0; j < m; j++) { // radius
+		for (int k = 0; k < n; k++) { // angle
+			insertValue(A,b,j,k,r_i,r_e,n,m,t_i,t_e);
 		}
 	}
-	printf("Finished loading system!\n");
+	// printf("Finished loading system!\n");
 
 	// A.printMatrix();
 
 	EquationSystemLU<double> e(A);
 
-	printf("Looking for solution!\n");
+	// // printf("Looking for solution!\n");
 	Matrix<double> result(e.solve(b));
-	printf("Solution found!\n");
+	// // printf("Solution found!\n");
 
 	result.printMatrix();
 
@@ -114,20 +114,33 @@ void insertValue(Matrix<double>& A, Matrix<double>& b, int j, int k, double r_i,
 
 	// cout << "j: " << j << " k: " << k << " m: " << m << " n: " << n << endl;
 
-	double dO = 2*M_PI / (n+1);
-	double dR = (r_e - r_i) / m;
+	double dO = 2*M_PI / n;
+	double dR = (r_e - r_i) / (m - 1);
 
-	int r = k * (m - 1) + (j - 1);
+	int r = j * n + k;
+	// int r = k * (m - 1) + (j - 1);
 	double r_j = r_i + j*dR;
+
+	cout << "j: " << j << " k: " << k << " m: " << m << " n: " << n << " r: " << r << endl;
+
+	if (j == 0) {
+		A(r,r) = 1;
+		b(r) = t_i[k];
+		return;
+	} else if (j == m - 1) {
+		A(r,r) = 1;
+		b(r) = t_e[k];
+		return;
+	}
 
 	// t_j,k
 	A(r,r) += - (2/pow(dR, 2)) + (1/(r_j*dR)) - (2/pow(r_j, 2)*pow(dO, 2));
 
 	// t_j,k+1, border case! k > n, angle = 0
-	A(r, ((k+1) % (n+1)) * (m-1) + (j-1)) += 1/(pow(r_j, 2)*pow(dO, 2));
+	A(r, ((k+1) % n) * (n-1) + j) += 1/(pow(r_j, 2)*pow(dO, 2));
 
 	// t_j,k-1, border case! k < 0 
-	A(r, ((k+1) % (n+1)) * (m-1) + (j-1)) += 1/(pow(r_j, 2)*pow(dO, 2));
+	A(r, ((k-1) % n) * (n-1) + j) += 1/(pow(r_j, 2)*pow(dO, 2));
 
 	// t_j-1,k
 	if (j == 1) { // inner circle
